94129516e7771f30527fb142d775852fddbdc4d1
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-12-30 19:59:10 +0100

    First commit

---
+++ contracts/Hamburguesa.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Hamburguesa is ERC20, Ownable {
+    constructor() ERC20(unicode"Hamburguesa de 1€ coin", "HDUEC") {}
+
+    function mint(address to, uint amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
